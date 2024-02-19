2abbc4ebda70740cfcdb5865c700cf2e1f5e6f03
RezaImany <43841254+RezaImany@users.noreply.github.com>
2024-02-19 14:39:57 +0000

    Create locustfile.py
    Optimized for reduce resource consumption

---
+++ locustfile.py
@@ -0,0 +1,26 @@
+from locust import HttpUser, task, between
+import requests
+import random
+#Put your stream master file address here
+master_url = "https://example.com/TEST/STREAM/master.m3u8"
+session_url = master_url.rsplit('/', 1)[0]
+master_response = self.session.get(master_url)
+master_content = master_response.text
+quality_list = [x for x in master_content.split('\n') if 'index.m3u8' in x]
+
+class HLSUser(HttpUser):
+    wait_time = between(1, 2)
+    session = requests.Session()
+
+    @task
+    def get_variant_playlist(self):
+        try:
+            rand_quality = random.choice(quality_list)
+            playlist_url = session_url + '/' + rand_quality
+            playlist_response = self.session.get(playlist_url)
+            playlist = playlist_response.text
+            ts = [x for x in playlist.split('\n') if x.endswith('.ts')]
+            ts_url = session_url + '/' + rand_quality.rsplit('/', 1)[0] + '/' + ts[-1]
+            self.client.get(ts_url)
+        except Exception as e:
+            print(f"Error occurred: {e}")
